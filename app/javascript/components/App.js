62498f137fac32fa65c3e8d3995f1704b70fb8c2
David Wu <david204code@gmail.com>
2020-04-12 20:18:36 +0100

    Able to sign user out using href instead of axios

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -8,10 +8,11 @@ class App extends React.Component {
   render () {
     return (
       <div>
-        {/* <button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="top" title="Tooltip on top">
-          Tooltip on top
-        </button>
-        <p class="display-1">This is the App.js</p> */}
+        {/* <div>
+          <a className="" rel="nofollow" data-method="delete" href="users/sign_out">
+            Log out
+          </a>
+        </div> */}
         <Map />
         <SignUp />
         <SignIn />
