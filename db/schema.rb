ad59eb4f0c1dbba4d04f91d03dbd7968d3d2a0a0
David Wu <david204code@gmail.com>
2020-04-14 00:26:56 +0100

    rails db:migrate and got the conversations and messages tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_12_223113) do
+ActiveRecord::Schema.define(version: 2020_04_13_232300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "conversations", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "helps", force: :cascade do |t|
     t.string "title"
@@ -29,6 +35,14 @@ ActiveRecord::Schema.define(version: 2020_04_12_223113) do
     t.index ["user_id"], name: "index_helps_on_user_id"
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.string "text"
+    t.bigint "conversation_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -42,4 +56,5 @@ ActiveRecord::Schema.define(version: 2020_04_12_223113) do
   end
 
   add_foreign_key "helps", "users"
+  add_foreign_key "messages", "conversations"
 end
