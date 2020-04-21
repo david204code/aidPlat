2867f5a021471376cdf45103dc19baa768a430a2
David Wu <david204code@gmail.com>
2020-04-22 00:54:45 +0100

    Attemption a new ways for SessionsController instead of Devise's and add skip_before_action :verify_authenticity_token

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { 
-    sessions: 'users/sessions' 
-  }
+  # devise_for :users, controllers: { 
+  #   sessions: 'users/sessions' 
+  # }
+  devise_for :users
+  resources :sessions
 
   resources :helps
   get 'helps', to: 'helps#index'
